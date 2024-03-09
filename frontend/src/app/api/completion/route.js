cdda7b7287d95d77a1f2982cefb71246f0c668c5
“Karthik144” <karthik.k.ramu@gmail.com>
2024-03-08 22:17:21 -0500

    Fixed bugs and added initial export features; need to fix formatting and content more

--- frontend/src/app/api/completion/route.js
+++ frontend/src/app/api/completion/route.js
@@ -7,23 +7,24 @@ const perplexity = new OpenAI({
 });
 
 export async function POST(req) {
-  // Extract the `messages` from the body of the request
-  const { tokens } = await req.json();
+  const requestBody = await req.json(); 
+  const promptObject = JSON.parse(requestBody.prompt);
+  const { tokens } = promptObject;
+
   const messages = [
     {
-        role: "system",
-        content:
-            "You are a crypto researcher that provides detailed reports about tokens. In the report, for each token, provide a summary, potential applications, and new non-price related updates.",
+      role: "system",
+      content:
+        "You are a crypto researcher that provides detailed reports about tokens. In the report, for each token, provide a summary, potential applications, and new non-price related updates.",
     },
     {
-        role: "user", 
-        content: `Given the following tokens, generate a detailed report. Tokens: ${tokens}`
-    }
-];
-
+      role: "user",
+      content: `Given the following tokens, generate a detailed report. Tokens: ${tokens}`,
+    },
+  ];
 
   console.log("Messages inside post:", messages);
-  // Request the OpenAI-compatible API for the response based on the prompt
+
   const response = await perplexity.chat.completions.create({
     model: "pplx-7b-chat",
     stream: true,
--- frontend/src/pages/report.js
+++ frontend/src/pages/report.js
@@ -1,19 +1,30 @@
-
 "use client";
-
 import React, { useEffect, useState, useCallback } from "react";
 import { Box, Paper } from "@mui/material";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import Skeleton from "@mui/material/Skeleton";
+import html2pdf from "html2pdf.js";
+import Stack from "@mui/material/Stack";
 import { useCompletion } from "ai/react";
-// const { generateReport } = require("./api/perplexity");
+import Button from "@mui/material/Button";
 
 export default function Report() {
   const [reportText, setReportText] = useState("");
   const { complete } = useCompletion({
     api: "/api/completion",
   });
+
+  const exportToPDF = () => {
+    const element = document.getElementById("content-to-export");
+    html2pdf(element, {
+      margin: 1,
+      filename: "exported_content.pdf",
+      image: { type: "jpeg", quality: 0.98 },
+      html2canvas: { scale: 2, logging: true, dpi: 192, letterRendering: true },
+      jsPDF: { unit: "in", format: "letter", orientation: "portrait" },
+    });
+  };
 
   const getTokens = async () => {
     const reportQuery = localStorage.getItem("reportQuery");
@@ -25,74 +36,74 @@ export default function Report() {
     return "";
   };
 
-  const generateReport = useCallback(async () => {
-    const tokenList = await getTokens(); // Retrieve tokens from getTokens
-    if (tokenList) {
-      const requestBody = { tokens: tokenList };
-      const completion = await complete(JSON.stringify(requestBody));
-      if (!completion) throw new Error("Failed to generate report");
+  // const generateReport = useCallback(async () => {
+  //   const tokenList = await getTokens(); 
+  //   console.log('Tokens inside call back:', tokenList); 
+  //   if (tokenList) {
+  //     const requestBody = { tokens: tokenList };
+  //     console.log("Request Body:", requestBody); 
+  //     const completion = await complete(JSON.stringify(requestBody));
+  //     if (!completion) throw new Error("Failed to generate report");
 
-      const report = completion;
-      console.log("COMPLETION:", completion); 
-      setReportText(report);
-    } else {
-      console.log("No tokens found");
-    }
-  }, [complete]);
+  //     const report = completion;
+  //     console.log("COMPLETION:", completion); 
+  //     setReportText(report);
+  //   } else {
+  //     console.log("No tokens found");
+  //   }
+  // }, [complete]);
 
-  useEffect(() => {
-    generateReport(); // Call generateReport once the component is mounted
-  }, [generateReport]); // Dependency array includes generateReport to ensure it's called when the function is available
+  // useEffect(() => {
+  //   generateReport(); 
+  // }, [generateReport]); 
 
   return (
-    <div className="flex min-h-screen flex-col items-start justify-between p-24">
-      <Grid container direction="column" style={{ paddingLeft: "50px" }}>
-        <Typography
-          variant="h4"
-          sx={{ textAlign: "left", pt: "70px", marginLeft: "60px" }}
-        >
-          Tokens with High Momentum
-        </Typography>
-
-        <div style={{ overflow: "auto", maxHeight: "60vh" }}>
-          <div>
-            {/* {!reportText ? (
-              <Box sx={{ width: 1200, marginLeft: "60px", marginTop: "15px" }}>
-                <Skeleton />
-                <Skeleton animation="wave" />
-                <Skeleton animation={false} />
-              </Box>
-            ) : (
-              <Box
-                overflow="auto"
-                paddingTop="20px"
-                paddingBottom="20px"
-                maxHeight="500px"
-              >
-                <Typography
-                  variant="body1"
-                  sx={{
-                    textAlign: "left",
-                    marginLeft: "60px",
-                    marginTop: "15px",
-                    maxWidth: "1200px",
-                  }}
-                  dangerouslySetInnerHTML={{ __html: reportText }}
-                />
-              </Box>
-            )} */}
-            {reportText && <div>{reportText}</div>}
-
-            {/* <form onSubmit={handleSubmit}>
-              <input
-                value={input}
-                placeholder="Say something..."
-                onChange={handleInputChange}
-              />
-            </form> */}
-          </div>
-        </div>
+    <div
+      id="content-to-export"
+      className="min-h-screen flex flex-col justify-between p-24"
+    >
+      <Grid container justifyContent="space-between" alignItems="flex-start">
+        <Grid item>
+          <Typography variant="h4" sx={{ pt: "70px", ml: "60px" }}>
+            Tokens with High Momentum
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={exportToPDF}
+            sx={{ mt: "70px", mr: "60px", height: "36px" }} // Adjusted height here
+          >
+            Export to PDF
+          </Button>
+        </Grid>
       </Grid>
+
+      <div style={{ overflow: "auto", maxHeight: "60vh" }}>
+        <div>
+          {!reportText ? (
+            <Box sx={{ width: 1200, ml: "60px", mt: "15px" }}>
+              <Skeleton />
+              <Skeleton animation="wave" />
+              <Skeleton animation={false} />
+            </Box>
+          ) : (
+            <Box overflow="auto" pt="20px" pb="20px" maxHeight="500px">
+              <Typography
+                variant="body1"
+                sx={{
+                  textAlign: "left",
+                  ml: "60px",
+                  mt: "15px",
+                  maxWidth: "1200px",
+                }}
+                dangerouslySetInnerHTML={{ __html: reportText }}
+              />
+            </Box>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
